e48e74b61d134d35c7571873126c6cc123905db9
AF <gulabsinghrajput008@gmail.com>
2025-03-06 10:36:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,71 +2,39 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-
-
-
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "0"))  # Default value set to 0
 API_HASH = os.environ.get("API_HASH", "")
-
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "7170452349"))
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
 
-
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))  # Default value set to 0
 FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "")  # पहला चैनल (ज़रूरी सब्सक्रिप्शन)
 FORCE_SUB_CHANNEL2 = os.environ.get("FORCE_SUB_CHANNEL2", "")  # दूसरा चैनल (Request Verification)
 
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600"))  # auto delete in seconds
 
-
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
-
-
-PORT = os.environ.get("PORT", "8080")
+PORT = int(os.environ.get("PORT", "8080"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-
-
 try:
-    ADMINS=[7170452349]
-    for x in (os.environ.get("ADMINS", "7170452349").split()):
+    ADMINS = [7170452349]
+    for x in os.environ.get("ADMINS", "7170452349").split():
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
-
-
-
-
-
-
-
-
+    raise Exception("Your Admins list does not contain valid integers.")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
+PROTECT_CONTENT = os.environ.get("PROTECT_CONTENT", "False").lower() == "true"
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "True").lower() == "true"
 
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
-
-
-
-
-
-
-
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
 START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
-
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
-
-
-
-
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(7170452349)
@@ -88,14 +56,8 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
-
-
-
-
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
